1c73aa7a6bba423e92f3cae396e6b9f59b17a9af
John Gilday <jgilday01@gmail.com>
2021-02-16 07:52:09 -0500

    use JSX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = {
+  name: "Madrid",
+  country: "Spain"
+};
+
 ReactDOM.render(
-  React.createElement("div", { style: { color: 'blue' } }, React.createElement("h1", null, "Hello") ),
+  <h1 id="heading" className="cool-text">{city.name} is in {city.country}</h1>,
   document.getElementById('root')
 );
