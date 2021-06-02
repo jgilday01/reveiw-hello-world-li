cf218d207f9210b379fbc1ad3dbfcdabef96047a
John Gilday <jgilday01@gmail.com>
2021-06-02 18:21:51 -0400

    list

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+const lakeList = [
+  { id: 1, name: "Echo", trialhead: "Echo" },
+  { id: 2, name: "Maud", trialhead: "Writes" },
+  { id: 3, name: "Velma", trialhead: "Bayview" }
+];
 
-function Lake({name}) {
-  return (
-    <h1>{name}</h1>
-  )
 
-}
 
-function App() {
+function App({ lakes }) {
   return (
     <div>
-      <Lake name="Lake Tahoe" />
-      <Lake name="Angora Lake" />
-      <Lake name="Shirley Lake" />
+      {lakes.map(lake => (
+        <div key={lake.id}>
+          <h2>{lake.name}</h2>
+          <p>Access: {lake.trialhead}</p>
+        </div>
+      ))}
     </div>
   )
 
@@ -24,5 +27,6 @@ function App() {
 
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <App lakes={lakeList} />,
+  document.getElementById('root')
 );
