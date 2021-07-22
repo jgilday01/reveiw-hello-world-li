a898c9563cd12cb4b2d9ecfb18610823e3470702
John Gilday <jgilday01@gmail.com>
2021-07-22 07:44:38 -0400

    useState

--- src/index.js
+++ src/index.js
@@ -5,15 +5,25 @@ import './index.css';
 
 
 function App() {
-  const [status, setStatus] = useState("Open")
+  const [year, setYear] = useState(2050);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
 
   return (
-    <div>
-      <h1>Status {status}</h1>
-      <button onClick={() => setStatus("Open")}>Open</button>
-      <button onClick={() => setStatus("Back in 5")}>Break</button>
-      <button onClick={() => setStatus("Closed")}>Closed</button>
-    </div>
+    <>
+      <div>
+        <h1>Year:{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Manager</button>
+        <h1>Manger on duty:{manager}</h1>
+        <button onClick={() => setManager("Rachel")}>New Manager</button>
+      </div>
+      <div>
+        <h1>Status {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
+    </>
   )
 
 }
